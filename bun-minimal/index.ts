36beed8d9e92fcaec38c858d5f17ba8ef91e4c5c
Arlan Lloyd <arlanlloyd@gmail.com>
2025-12-09 04:48:41 +0000

    finish helloworld

--- bun-minimal/index.ts
+++ bun-minimal/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import figlet from 'figlet';
+import index from './index.html';
+
+const server = Bun.serve({
+  port: 8025,
+  routes: {
+    "/": index, 
+    "/f": () => { 
+      const body = figlet.textSync('Bun!'); 
+      return new Response(body); 
+    } 
+  }
+});
+
+console.log(`Listening on ${server.url}`);
