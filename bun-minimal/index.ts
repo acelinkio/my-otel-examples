9d6ee4d4b4969fdf647a3d01cb11d6ceecd87012
Arlan Lloyd <arlanlloyd@gmail.com>
2025-12-09 05:54:34 +0000

    working example with logger

--- bun-minimal/index.ts
+++ bun-minimal/index.ts
@@ -1,6 +1,31 @@
 import figlet from 'figlet';
 import index from './index.html';
 
+import {
+  LoggerProvider,
+  SimpleLogRecordProcessor,
+} from '@opentelemetry/sdk-logs';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-grpc';
+
+const collectorOptions = {
+  // url is optional and can be omitted - default is http://localhost:4317
+  // Unix domain sockets are also supported: 'unix:///path/to/socket.sock'
+  url: 'http://otel-collector.opentelemetry.svc.cluster.local:4317',
+};
+
+const loggerExporter = new OTLPLogExporter(collectorOptions);
+const loggerProvider = new LoggerProvider({
+  processors: [new SimpleLogRecordProcessor(loggerExporter)]
+});
+
+['SIGINT', 'SIGTERM'].forEach(signal => {
+  process.on(signal, () => loggerProvider.shutdown().catch(console.error));
+});
+
+// logging
+const logger = loggerProvider.getLogger('example-logger');
+logger.emit({ body: 'example-log' });
+
 const server = Bun.serve({
   port: 8025,
   routes: {
